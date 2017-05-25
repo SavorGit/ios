0c2105263342525e227ab474f21049512d159086
郭春城 <guochuncheng@guochunchengdeMacBook-Pro-2.local>
2017-05-25 10:23:57 +0800

    no message

--- SavorX/SavorX/Controllers/HomePage/HSConnectViewController.m
+++ SavorX/SavorX/Controllers/HomePage/HSConnectViewController.m
@@ -182,7 +182,7 @@
 {
     HelpViewController * help = [[HelpViewController alloc] initWithURL:@"http://h5.rerdian.com/Public/html/help/helptwo.html"];
     help.title = @"连接步骤";
-    [self.navigationController  pushViewController:help  animated:NO];
+    [self.navigationController  pushViewController:help  animated:YES];
     [SAVORXAPI postUMHandleWithContentId:@"link_tv_help" key:nil value:nil];
 }
 
