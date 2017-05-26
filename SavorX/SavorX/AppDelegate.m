cb1bed36742172b8bf69803eff3e7cb5468b0eeb
郭春城 <guochuncheng@guochunchengdeMacBook-Pro-2.local>
2017-05-26 17:51:59 +0800

    在未切换wifi时，进入前台不进行检测

--- SavorX/SavorX/AppDelegate.m
+++ SavorX/SavorX/AppDelegate.m
@@ -34,6 +34,7 @@
 @interface AppDelegate ()<UITabBarControllerDelegate, UNUserNotificationCenterDelegate,SplashViewControllerDelegate, WMPageControllerDelegate >
 
 @property (nonatomic, assign) BOOL is3D_Touch;
+@property (nonatomic, copy) NSString * ssid;
 
 @end
 
@@ -643,9 +644,11 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     
-    [NSObject cancelPreviousPerformRequestsWithTarget:[GCCDLNA defaultManager] selector:@selector(startSearchDevice) object:nil];
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    if ([GlobalData shared].scene == RDSceneHaveRDBox) {
+        self.ssid = [Helper getWifiName];
+    }else{
+        self.ssid = @"";
+    }
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -663,6 +666,12 @@
     if (self.is3D_Touch) {
         self.is3D_Touch = NO;
         return;
+    }
+    
+    if (!isEmptyString(self.ssid) && [Helper getWifiName]) {
+        if ([self.ssid isEqualToString:[Helper getWifiName]]) {
+            return;
+        }
     }
     
     if ([self.window.rootViewController isKindOfClass:[LGSideMenuController class]]) {
