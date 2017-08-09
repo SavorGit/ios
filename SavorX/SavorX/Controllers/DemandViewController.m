e8260c1d9d3e287ed267e3ebdc09311457d92de4
beimingyouyu0823 <haipeng0823@126.com>
2017-08-09 12:18:53 +0800

    点播页面增加推荐分享

--- SavorX/SavorX/Controllers/DemandViewController.m
+++ SavorX/SavorX/Controllers/DemandViewController.m
@@ -21,9 +21,14 @@
 #import "HotPopShareView.h"
 #import "RDVideoHeaderView.h"
 
+#import "HSImTeRecommendRequest.h"
+#import "HotTopicShareView.h"
+#import "RDFavoriteTableViewCell.h"
+#import "RDAlertView.h"
+
 #define BOTTOMHEIGHT 50.f
 
-@interface DemandViewController ()<UIWebViewDelegate, UIScrollViewDelegate>
+@interface DemandViewController ()<UIWebViewDelegate, UIScrollViewDelegate,UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic, strong) UIWebView * webView; //加载Html网页视图
 @property (nonatomic, strong) UIView *playBackView; //播放空间的背景View
@@ -43,12 +48,20 @@
 @property (nonatomic, strong) UIView *maskingView;
 @property (nonatomic, assign) BOOL isComplete; //内容是否阅读完整
 
+@property (nonatomic, assign) BOOL isOnlyVideo; //是否是纯视频
+@property (nonatomic, strong) UITableView * baseTableView; //纯视频展示视图
+@property (nonatomic, strong) UIView * testView;   //底部视图
+@property (nonatomic, strong) UITableView * tableView; //表格展示视图
+@property (nonatomic, strong) NSMutableArray * dataSource; //数据源
+@property (nonatomic, assign) BOOL hasWebObserver;
+
 @end
 
 @implementation DemandViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.dataSource = [NSMutableArray new];
     
     _isComplete = NO;
     [self createUI];
@@ -210,7 +223,8 @@
         make.centerY.equalTo(self.backImageView);
     }];
     
-    [self createWebView];
+    [self refreshPage];
+//    [self createWebView];
     [self createBottomView];
     [self addBottomEvent];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -245,34 +259,6 @@
         [self.navigationController popViewControllerAnimated:YES];
         return;
     }
-    
-//    UINavigationController * na = [Helper getRootNavigationController];
-//    int j = (int)na.viewControllers.count - 2;
-//    if (j < 0) {
-//        j = 0;
-//    }
-    
-//    UIViewController * vc = na.viewControllers[j];
-//    if ([vc  isKindOfClass:[WebViewController class]] ) {
-////        [self.navigationController popToViewController:vc animated:YES];
-//    }else if ([vc  isKindOfClass:[WMPageController class]]){
-//        [self.navigationController popViewControllerAnimated:YES];
-//    }
-    
-//    UIViewController * vc = na.viewControllers;
-//    if ([vc  isKindOfClass:[WebViewController class]] ) {
-//        [self.navigationController popToViewController:vc animated:YES];
-//    }else if ([vc  isKindOfClass:[WMPageController class]]){
-//        WebViewController *weVc = [[WebViewController alloc] init];
-//        weVc.image = self.backImageView.image;
-//        weVc.model = self.model;
-//        weVc.isFormDemand = YES;
-//        [self.navigationController pushViewController:weVc animated:YES];
-//        weVc.coFromWebView = ^(NSDictionary *parmDic){
-//            [self restartVod];
-//        };
-//    }
-    
      [self.navigationController popViewControllerAnimated:YES];
     
 }
@@ -377,18 +363,6 @@
             make.left.mas_equalTo(0);
             make.size.mas_equalTo(view.frame.size);
         }];
-        
-//        UILabel *contentTitleLab = [[UILabel alloc] initWithFrame:CGRectMake(0, 0,kScreen_Width - 20, 30)];
-//        contentTitleLab.font = [UIFont systemFontOfSize:16.f];
-//        contentTitleLab.textColor = [UIColor blackColor];
-//        contentTitleLab.text = self.title;
-//        self.maximumLabel.textAlignment = NSTextAlignmentLeft;
-//        [self.view addSubview:contentTitleLab];
-//        [contentTitleLab mas_makeConstraints:^(MASConstraintMaker *make) {
-//            make.bottom.equalTo(self.backImageView).offset(30);
-//            make.left.mas_equalTo(15);
-//            make.width.mas_equalTo(kScreen_Width - 15);
-//        }];
     }
 }
 
@@ -836,25 +810,424 @@
     return YES;
 }
 
+- (void)refreshPage
+{
+    if (self.model.type == 4) {
+        
+        self.isOnlyVideo = YES;
+        [self refreshWithOnlyVideo];
+        
+    }else{
+        
+        self.isOnlyVideo = NO;
+        [self refreshWithVideo];
+    }
+    [self setUpDatas];
+}
+
+#pragma mark - 非纯视频的页面布局
+- (void)refreshWithVideo
+{
+    if (self.baseTableView.superview) {
+        self.baseTableView.delegate = nil;
+        self.baseTableView.dataSource = nil;
+        [self.baseTableView removeFromSuperview];
+    }
+    
+    if (!self.webView) {
+        [self createWebViewWithVideo];
+    }
+    if (!self.webView.superview) {
+        [self.view addSubview:self.webView];
+        [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.backImageView.mas_bottom);
+            make.left.mas_equalTo(0);
+            make.width.mas_equalTo(kMainBoundsWidth);
+            make.bottom.mas_equalTo(0);
+        }];
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+        self.webView.scrollView.delegate = self;
+        self.webView.delegate = self;
+        self.navigationController.interactivePopGestureRecognizer.delegate = (id)self.webView;
+    }
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
+    [self.dataSource removeAllObjects];
+    [self.tableView reloadData];
+    if (!isEmptyString(self.model.contentURL)) {
+        NSURLRequest * request = [NSURLRequest requestWithURL:[NSURL URLWithString:[[self.model.contentURL stringByAppendingString:@"?location=newRead&app=inner"] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]]];
+        [self.webView loadRequest:request];
+        [MBProgressHUD showWebLoadingHUDInView:self.webView];
+        if (!self.webView.superview) {
+            [self addObserver];
+        }
+    }
+}
+
+- (void)createWebViewWithVideo
+{
+    //初始化webView
+    self.webView = [[UIWebView alloc] init];
+    self.webView.backgroundColor = [UIColor clearColor];
+    self.webView.opaque = NO;
+    
+    [self.webView.scrollView addSubview:self.testView];
+}
+
+#pragma mark - 纯视频的页面布局
+//刷新当前的纯视频视图
+- (void)refreshWithOnlyVideo
+{
+    if (self.webView.superview) {
+        //移除非纯视频播放展示的webView
+        [self.webView removeFromSuperview];
+        [self.testView removeFromSuperview];
+        self.webView.delegate = nil;
+        self.webView.scrollView.delegate = nil;
+        self.tableView.delegate = nil;
+        self.tableView.dataSource = nil;
+        [self removeObserver];
+    }
+    
+    //添加纯视频播放页面的推荐列表
+    if (!self.baseTableView) {
+        [self createTableViewWithOnlyVideo];
+    }
+    if (!self.baseTableView.superview) {
+        [self.view addSubview:self.baseTableView];
+        [self.baseTableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(self.backImageView.mas_bottom).offset(0);
+            make.left.mas_equalTo(0);
+            make.right.mas_equalTo(0);
+            make.bottom.mas_equalTo(0);
+        }];
+        self.baseTableView.dataSource = self;
+        self.baseTableView.delegate = self;
+    }
+    
+    //添加用户右滑退出该页面的手势操作
+    self.navigationController.interactivePopGestureRecognizer.delegate = (id)self.baseTableView;
+    RDVideoHeaderView * headerView = (RDVideoHeaderView *)self.baseTableView.tableHeaderView;
+    [headerView reloadWithModel:self.model];
+    
+    [self.dataSource removeAllObjects];
+    [self.baseTableView reloadData];
+}
+
+- (void)createTableViewWithOnlyVideo
+{
+    self.baseTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, kMainBoundsWidth, kMainBoundsHeight) style:UITableViewStyleGrouped];
+    self.baseTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.baseTableView.clipsToBounds = YES;
+    self.baseTableView.backgroundColor = UIColorFromRGB(0xf6f2ed);
+    
+    RDVideoHeaderView * headerView = [[RDVideoHeaderView alloc] init];
+    self.baseTableView.tableHeaderView = headerView;
+}
+
+#pragma mark - 初始化下方推荐数据
+- (void)setUpDatas{
+    
+    [HSImTeRecommendRequest cancelRequest];
+    HSImTeRecommendRequest * request = [[HSImTeRecommendRequest alloc] initWithArticleId:self.model.artid];
+    [request sendRequestWithSuccess:^(BGNetworkRequest * _Nonnull request, id  _Nullable response) {
+        
+        [self.dataSource removeAllObjects];
+        
+        NSDictionary *dic = (NSDictionary *)response;
+        NSArray *resultArr = [dic objectForKey:@"result"];
+        
+        for (int i = 0; i < resultArr.count; i ++) {
+            CreateWealthModel *welthModel = [[CreateWealthModel alloc] initWithDictionary:resultArr[i]];
+            welthModel.acreateTime = welthModel.updateTime;
+            [self.dataSource addObject:welthModel];
+        }
+        // 当返回有推荐数据时调用
+        if (self.dataSource.count > 0) {
+            if (self.isOnlyVideo) {
+                [self.baseTableView reloadData];
+            }else{
+                [self footViewShouldBeReset];
+                [self.tableView reloadData];
+            }
+        }
+        
+        if (self.isOnlyVideo) {
+            RDVideoHeaderView * headerView = (RDVideoHeaderView *)self.baseTableView.tableHeaderView;
+            [headerView needRecommand:self.dataSource.count != 0];
+        }
+        
+    } businessFailure:^(BGNetworkRequest * _Nonnull request, id  _Nullable response) {
+        
+        if (self.isOnlyVideo) {
+            RDVideoHeaderView * headerView = (RDVideoHeaderView *)self.baseTableView.tableHeaderView;
+            [headerView needRecommand:self.dataSource.count != 0];
+        }
+        
+    } networkFailure:^(BGNetworkRequest * _Nonnull request, NSError * _Nullable error) {
+        
+        if (self.isOnlyVideo) {
+            RDVideoHeaderView * headerView = (RDVideoHeaderView *)self.baseTableView.tableHeaderView;
+            [headerView needRecommand:self.dataSource.count != 0];
+        }
+        
+    }];
+}
+
+- (void)addObserver
+{
+    if (!self.hasWebObserver) {
+        self.hasWebObserver = YES;
+        [self.webView.scrollView addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew context:nil];
+    }
+}
+
+- (void)removeObserver
+{
+    if (self.hasWebObserver) {
+        self.hasWebObserver = NO;
+        [self.webView.scrollView removeObserver:self forKeyPath:@"contentSize" context:nil];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    [MBProgressHUD hiddenWebLoadingInView:self.webView];
+    if ([keyPath isEqualToString:@"contentSize"]) {
+        [self footViewShouldBeReset];
+    }
+}
+
+- (void)footViewShouldBeReset
+{
+    [self removeObserver];
+    
+    if (self.testView.superview) {
+        [self.testView removeFromSuperview];
+    }
+    //TableView的高度
+    CGFloat tabHeight = 0;
+    if (self.dataSource.count != 0) {
+        tabHeight = self.dataSource.count *96 + 48;
+    }
+    
+    [self.tableView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(tabHeight);
+    }];
+    
+    //底部View总高度
+    CGFloat theight = tabHeight + 115 + 30;
+    if (self.dataSource.count != 0) {
+        theight += 8;
+    }
+    
+    CGFloat cSizeheight = self.webView.scrollView.contentSize.height;
+    CGRect frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, theight);
+    CGSize contentSize = self.webView.scrollView.contentSize;
+    //底部View与顶部网页的间隔为0
+    frame.origin.y = cSizeheight;
+    self.testView.frame = frame;
+    [self.webView.scrollView addSubview:self.testView];
+    [self.webView.scrollView setContentSize:CGSizeMake(contentSize.width, contentSize.height + theight + 50)];
+    
+    [self addObserver];
+    
+    //不是纯视频类型添加分享部分
+    if (self.model.type != 4) {
+        [self shareBoardByDefined];
+    }
+}
+
+- (void)shareBoardByDefined {
+    
+    if ([self.testView viewWithTag:1000]) {
+        [[self.testView viewWithTag:1000] removeFromSuperview];
+    }
+    HotTopicShareView *shareView = [[HotTopicShareView alloc] initWithModel:self.model andVC:self andCategoryID:self.categroyID andY:30];
+    shareView.tag = 1000;
+    [self.testView addSubview:shareView];
+    
+}
+
+#pragma mark -- 懒加载
+- (UITableView *)tableView
+{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _tableView.backgroundColor = UIColorFromRGB(0xf6f2ed);
+        _tableView.backgroundView = nil;
+        _tableView.scrollEnabled = NO;
+        [self.testView addSubview:_tableView];
+        
+        CGFloat diatanceToTop = 115+30+8;
+        // 纯视频类型去除上边分享部分
+        if (self.model.type == 4) {
+            diatanceToTop = 8;
+        }
+        CGFloat tabHeiht;
+        if (self.dataSource.count == 0) {
+            tabHeiht = 0;
+        }else{
+            tabHeiht = self.dataSource.count *96 +48;
+        }
+        [_tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.mas_equalTo(kMainBoundsWidth);
+            make.height.mas_equalTo(tabHeiht);
+            make.top.mas_equalTo(diatanceToTop);
+            make.left.mas_equalTo(0);
+        }];
+        
+        UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 48)];
+        headView.backgroundColor = UIColorFromRGB(0xf6f2ed);
+        UILabel *recommendLabel = [[UILabel alloc] init];
+        recommendLabel.frame = CGRectMake(15, 10, 100, 30);
+        recommendLabel.textColor = UIColorFromRGB(0x922c3e);
+        recommendLabel.font = kPingFangRegular(15);
+        recommendLabel.text = RDLocalizedString(@"RDString_RecommendForYou");
+        recommendLabel.textAlignment = NSTextAlignmentLeft;
+        [headView addSubview:recommendLabel];
+        _tableView.tableHeaderView = headView;
+    }
+    
+    return _tableView;
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataSource.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellID = @"videoTableCell";
+    RDFavoriteTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil) {
+        cell = [[RDFavoriteTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.backgroundColor = [UIColor clearColor];
+    cell.contentView.backgroundColor = [UIColor clearColor];
+    
+    //最后一条分割线隐藏
+    if (indexPath.row == self.dataSource.count - 1) {
+        [cell setLineViewHidden:YES];
+    }else{
+        [cell setLineViewHidden:NO];
+    }
+    
+    CreateWealthModel * model = [self.dataSource objectAtIndex:indexPath.row];
+    [cell configWithModel:model];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 96.f;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    CreateWealthModel * model = [self.dataSource objectAtIndex:indexPath.row];
+    
+    if ([GlobalData shared].isBindRD) {
+        
+        [self demandVideoWithModel:model force:0];
+        
+    }else{
+        
+        [[RDHomeStatusView defaultView] scanQRCode];
+    }
+    
+}
+
+- (UIView *)testView
+{
+    if (!_testView) {
+        _testView = [[UIView alloc] initWithFrame:CGRectMake(0, 0,kMainBoundsWidth, 100)];
+        _testView.backgroundColor = UIColorFromRGB(0xf6f2ed);
+        _testView.clipsToBounds = YES;
+    }
+    return _testView;
+}
+
+- (void)demandVideoWithModel:(CreateWealthModel *)model force:(NSInteger)force{
+    
+    RDInteractionLoadingView * hud = [[RDInteractionLoadingView alloc] initWithView:self.view title:RDLocalizedString(@"RDString_Demanding")];
+    [SAVORXAPI demandWithURL:STBURL name:model.name type:1 position:0  force:force success:^(NSURLSessionDataTask *task, NSDictionary *result) {
+        if ([[result objectForKey:@"result"] integerValue] == 0) {
+            
+            self.model = model;
+            [SAVORXAPI successRing];
+            [self recommendToScreenSuccess];
+            
+        }else if ([[result objectForKey:@"result"] integerValue] == 4) {
+            
+            NSString *infoStr = [result objectForKey:@"info"];
+            RDAlertView *alertView = [[RDAlertView alloc] initWithTitle:RDLocalizedString(@"RDString_AlertWithScreen") message:[NSString stringWithFormat:@"%@%@%@", RDLocalizedString(@"RDString_ScreenContinuePre"), infoStr, RDLocalizedString(@"RDString_ScreenContinueSuf")]];
+            RDAlertAction * action = [[RDAlertAction alloc] initWithTitle:RDLocalizedString(@"RDString_Cancle") handler:^{
+                [SAVORXAPI postUMHandleWithContentId:@"to_screen_competition_hint" withParmDic:@{@"to_screen_competition_hint" : @"cancel",@"type" : @"vod"} ];
+            } bold:NO];
+            RDAlertAction * actionOne = [[RDAlertAction alloc] initWithTitle:RDLocalizedString(@"RDString_ContinueScreen") handler:^{
+                [self demandVideoWithModel:model force:1];
+                [SAVORXAPI postUMHandleWithContentId:@"to_screen_competition_hint" withParmDic:@{@"to_screen_competition_hint" : @"ensure",@"type" : @"vod"} ];
+            } bold:YES];
+            [alertView addActions:@[action,actionOne]];
+            [alertView show];
+            
+        }else{
+            [SAVORXAPI showAlertWithMessage:[result objectForKey:@"info"]];
+        }
+        [hud hidden];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        [hud hidden];
+        [MBProgressHUD showTextHUDwithTitle:DemandFailure];
+    }];
+}
+
+// 点击推荐投屏成功
+- (void)recommendToScreenSuccess{
+    
+    self.title = self.model.title;
+    self.playSilder.maximumValue = self.model.duration;
+    NSInteger pLayDurationInt = self.model.duration; // some duration from the JSON
+    NSInteger playMinutesInt = pLayDurationInt / 60;
+    NSInteger playSecondsInt = pLayDurationInt % 60;
+    NSString *playTimeStr = [NSString stringWithFormat:@"%02ld:%02ld", (long)playMinutesInt, (long)playSecondsInt];
+    self.maximumLabel.text = playTimeStr;
+    
+    [[RDHomeStatusView defaultView] startScreenWithViewController:self withStatus:RDHomeStatus_Demand];
+    [SAVORXAPI postUMHandleWithContentId:@"home_click_bunch_video" key:nil value:nil];
+    
+    [self.backImageView sd_setImageWithURL:[NSURL URLWithString:self.model.imageURL] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    [self refreshPage];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self createTimer];
+    });
+}
+
 - (void)dealloc
 {
     NSLog(@"点播页面释放了");
     [[NSNotificationCenter defaultCenter] removeObserver:self name:RDBoxQuitScreenNotification object:nil];
+    
+    [self removeObserver];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
