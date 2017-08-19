7d2fccb9858a76ad631a8e6f87669d257e91bffc
beimingyouyu0823 <haipeng0823@126.com>
2017-08-19 18:05:18 +0800

    修改图集类

---
+++ SavorX/SavorX/Views/Views_3_0/ImageAtlasView/ImageRecomendViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ImageRecomendViewCell.h
+//  SavorX
+//
+//  Created by 王海朋 on 2017/8/19.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageRecomendViewCell : UICollectionViewCell
+
+- (void)configModelData:(NSMutableArray *)modelArr andIsPortrait:(BOOL)isPortrait;
+
+@end
